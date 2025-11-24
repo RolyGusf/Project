f197f174b3326db09891bb63c47db5ff27fd961b
RolyGusf <rolyg@usf.edu>
2025-11-23 23:10:08 -0500

    Fixing format

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 print("Welcome to my Python program!")
 hours = input("How many hours did you study today? ")
-weekly_hours = hours * 7
-print(f"You are on track to study {weekly_hours} hours this week.")
 try:
     hours = float(hours)
 except ValueError:
     print("Please enter a valid number.")
-    exit()
+    exit
+    weekly_hours = hours * 7
+print(f"You are on track to study {weekly_hours} hours this week.")
